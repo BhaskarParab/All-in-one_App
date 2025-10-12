ad712309701807e19f3c486a7e45fc65615b64a4
Bhaskar <Bhaskarrparab@gmail.com>
2025-10-12 20:36:20 +0530

    weather app api fetch

---
+++ weather.js
@@ -0,0 +1,38 @@
+const celcius = document.getElementById('celsius')
+const input = document.getElementById('input')
+const error = document.getElementById('error')
+
+function fetchData(){
+    const APIkey = '2c1d3d7836777b03a149d061b9c4b5dd'
+
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${APIkey}&q=${input.value}&units=metric`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        if(data.cod != 200){
+            error.innerHTML = `Enter valid place`;
+            celcius.innerHTML = '';
+        return;
+        }
+        error.innerHTML = '';
+        console.log(data)
+        const current = data.list[0]
+        const weather = data.list[0].weather[0]
+        celcius.innerHTML = `<h1>${data.city.name}</h1>
+        <h1>${current.main.temp}°C</h1>
+        <h1>${weather.description}</h1>`
+
+    })
+    .catch((err) => { 
+        console.log(err)
+    })
+}
+
+input.addEventListener('keydown',(e) =>{
+    if(e.key === 'Enter'){
+        fetchData()
+        input.value = ''
+    }
+})
+
